9022755f5ac59cba9fc6307881a4b5fbf43937fd
Sanmoondee11 <sanmoon2001fu@gmail.com>
2025-03-07 15:27:55 +0900

    タスク追加機能モーダル画面遷移実装

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,16 @@
+//layouts/: ページ全体のレイアウトを担当するコンポーネント
+
+import React from 'react';
+
+const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <div style={{ margin: '0 auto', maxWidth: '800px', padding: '20px' }}>
+      <header>
+        <h1>TODOアプリ</h1>
+      </header>
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default MainLayout;
